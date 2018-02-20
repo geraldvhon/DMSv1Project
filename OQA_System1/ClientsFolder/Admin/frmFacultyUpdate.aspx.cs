3f511ad5726928f9a4058c5ee80c80896c9b3641
geraldvhon <geraldvhan@hotmail.com>
2018-02-20 16:19:04 +0800

    frmUpdateFaculty

--- OQA_System1/ClientsFolder/Admin/frmFacultyUpdate.aspx.cs
+++ OQA_System1/ClientsFolder/Admin/frmFacultyUpdate.aspx.cs
@@ -26,7 +26,7 @@ namespace OQA_System1.ClientsFolder.Web_ADMinFolder
 
 
         private void getValue_fromDb()
-        {
+        {   
             txtempno.Text = xempid;
             tblemp.EmpID = txtempno.Text;
             txtlname.Text = tblemp.sp_Faculty_Display2().Rows[0][1].ToString();
@@ -46,7 +46,30 @@ namespace OQA_System1.ClientsFolder.Web_ADMinFolder
             txtexpertin.Text = tblemp.sp_Faculty_Display2().Rows[0][15].ToString();
             drpF_Type.Text = tblemp.sp_Faculty_Display2().Rows[0][18].ToString();
             drpF_Rank.Text = tblemp.sp_Faculty_Display2().Rows[0][19].ToString();
-            
+        }
+
+
+        private void Update_Data()
+        {
+            tblemp.EmpID = txtempno.Text;
+            tblemp.Lname = txtlname.Text;
+            tblemp.Lname =  txtlname.Text ;
+            tblemp.Fname = txtfname.Text ;
+            tblemp.Mname =  txtmname.Text ;
+            tblemp.NkName = txtNkName.Text ;
+            tblemp.Gender = drpGender.Text ;
+            tblemp.Civil_Stat = drpCivilStatus.Text;
+            tblemp.Religion = drpReligion.Text ;
+            tblemp.Per_add = txtAddress.Text;
+            tblemp.Email = txtEmail.Text ;
+            tblemp.Telno = txtTelNo.Text ;
+            tblemp.Cp_no = txtCellNo.Text;
+            tblemp.Bday = txtBday.Text;
+            tblemp.Bplace = txtBplace.Text ;
+            tblemp.StatusCode = drpF_Status.Text;
+            tblemp.ExpertIn = txtexpertin.Text;
+            //tblemp. drpF_Type.Text;
+            //tblemp.Rank = drpF_Rank.Text;
 
         }
     }
